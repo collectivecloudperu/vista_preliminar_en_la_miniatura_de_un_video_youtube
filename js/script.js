3fef29332b4de035c0f1b794e1bc5e0c580eae46
Collective Cloud Peru <collectivecloudperu@gmail.com>
2015-06-29 17:58:57 -0500

    Vista Preliminar en la miniatura de un Video Youtube con jQuery
    Como hacer una Vista Preliminar en la miniatura de un Video Youtube con
    jQuery

---
+++ js/script.js
@@ -0,0 +1,76 @@
+
+(function($) {
+
+	$.fn.PreViewTube = function(opts) {
+
+		//Defaults
+		var settings = $.extend({
+			'interval' : 500,
+			'mode' : 'hover'
+		}, opts);
+
+		//Iterate over matched elements
+		return this.each(function() {
+
+			var $this = $(this);
+
+			var loop;
+
+			// get the image source
+			var imgSrc = $this.attr("src");
+			// get the image filename
+			var fileName = imgSrc.match(/(\w*)\.jpg$/);
+
+			// get the imgae number
+			if(fileName[1] == 'default') {
+				// if default set to 1
+				var num = 1;
+			} else {
+				var num = parseInt(fileName[1]);
+			}
+
+			// declare the loop
+			var infiniteLoop = null;
+
+			// check for mode [ hover || constant ]
+			if(settings.mode == 'constant') {
+
+				loop = setInterval(function() {
+					// loop - one, two, trí
+					if(num == 3) {
+						num = 0;
+					} else {
+						num++;
+					}
+					// set the image source on the element
+					$this.attr("src", imgSrc.replace(/(\d\.jpg|\w*\.jpg)$/, +num + '.jpg'));
+
+				}, settings.interval);
+				// interval/delay loop
+
+			} else {// hover mode
+
+				$this.hover(function() {
+
+					loop = setInterval(function() {
+						// loop - one, two, trí
+						if(num == 3) {
+							num = 0;
+						} else {
+							num++;
+						}
+						// set the image source on the element
+						$this.attr("src", imgSrc.replace(/(\d\.jpg|\w*\.jpg)$/, +num + '.jpg'));
+
+					}, settings.interval);
+
+				}, function() {// mouse out
+					// stop loop
+					clearInterval(loop);
+
+				});
+			}
+
+		});
+	};
+})(jQuery);
